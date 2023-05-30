bb6672c5a2dbcada08aee6dbb4848f94d692a820
Juan Romero Peralta <juanmaromeroperalta9@gmail.com>
2023-05-30 16:01:28 -0500

    ultCorreccion

--- public/js/cart.js
+++ public/js/cart.js
@@ -43,11 +43,6 @@ const total = () => {
     return totalPrecio
 }
 
-// const generateRandomNumber = () => {
-//     const min = 100000;
-//     const max = 999999;
-//     return Math.floor(Math.random() * (max - min +1)) + min;
-//   };
 const generateRandomNumber = () =>{
     let numberRandom = ''
     for (let i = 0; i < 6; i++) {
